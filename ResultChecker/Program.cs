d7e27b6b504ce09cfba36e706a8f837cef091d3b
muhdamean <muhdamean@gmail.com>
2021-08-26 08:03:11 +0100

    remove docker file

--- ResultChecker/Program.cs
+++ ResultChecker/Program.cs
@@ -21,6 +21,7 @@ namespace ResultChecker
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                   // webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
                 });
     }
 }
