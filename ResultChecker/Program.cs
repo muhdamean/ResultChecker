229de8d29e8ce479ab1eec2960e891dfd772db04
muhdamean <muhdamean@gmail.com>
2021-10-05 22:25:00 +0100

    add docker file and UseUrls in program.cs

--- ResultChecker/Program.cs
+++ ResultChecker/Program.cs
@@ -21,7 +21,7 @@ namespace ResultChecker
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                   // webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
+                    webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
                 });
     }
 }
